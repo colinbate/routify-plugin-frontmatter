a3233b8c28a8421234b92330194d12dde622685f
Colin Bate <colin@colinbate.com>
2020-06-01 20:42:42 -0300

    Implement custom parser configurability

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-### Usage
+# Routify Frontmatter Plugin
 
-In your config or package.json insert
+Extracts frontmatter from Markdown or MDSveX files. Supports YAML by default, but can be configured for any type of delimiters or parser.
+
+## Usage
+
+In your `routify.config.js` or `package.json` insert
 
 ```json
   "routify": {
@@ -13,9 +17,34 @@ In your config or package.json insert
 
 Frontmatter metadata can be found in ``meta.frontmatter`` of the respective file.
 
-To change default extensions (md and svx)
-``"routify-plugin-frontmatter": { "extensions": ["md"] }``
+To change extensions (default: `md` and `svx`)
+
+```json
+{ "routify-plugin-frontmatter": { "extensions": ["md"] } }
+```
+
+### Custom Parser
+
+To set custom a custom parser, you will need to use the `routify.config.js` config option as you will need to export a function. For example, if you want to parse TOML frontmatter, you might create a `routify.config.js` like this:
+
+```js
+const toml = require('@iarna/toml/parse-string');
+
+module.exports = {
+  plugins: {
+    'routify-plugin-frontmatter': {
+      delimiter: '+++',
+      parse(raw) {
+        return toml(raw);
+      }
+    }
+  }
+};
+```
+
+The `delimiter` option can take an array of strings, or an array of or single object of the shape: `{open: "<<<", close: ">>>"}` to support different open and close delimiters. If it matches one of the delimiter pairs, it will pass the raw text to the `parse()` method. It also passes the open and closing delimiter as the second and third parameter to `parse` so you can use that to determine the type of parsing needed. The `parse` method can be `async` if needed.
 
 
-#### Note
+## Note
+
 This plugin does not strip frontmatter from your file. It only reads it and adds it to your metadata.
--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 const YAML = require('yaml')
-const defaults = { extensions: ['md', 'svx'] }
+const fs = require('fs')
+const read = (filepath) => new Promise((resolve, reject) => {
+    fs.readFile(filepath, 'utf8', (err, content) => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve(content)
+        }
+    })
+})
+const escape = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d')
+const defaults = {
+    extensions: ['md', 'svx'],
+    delimiter: '---',
+    parse: raw => YAML.parse(raw)
+}
 
 module.exports = function (middlewares, pl, options) {
     options = { ...defaults, ...options }
-    
     // insert frontmatter metadata after meta is created
     const index = middlewares.findIndex(mw => mw.name === 'applyMetaToFiles')
     middlewares.splice(index + 1, 0, {
-        name: "routify-plugin-frontmatter",
+        name: 'routify-plugin-frontmatter',
         middleware: middleware
+    })
+
+    const matchers = [].concat(options.delimiter).map(d => {
+        const open = d.open || d
+        const close = d.close || d
+        const pattern = new RegExp(`^${escape(open)}((?:.|[\\s\\S])+?)^${escape(close)}`, 'm');
+        return { open, close, pattern }
     })
 
     return middlewares
 
-    async function middleware(payload) { await walkFiles(payload.tree) }
+    async function middleware(payload) { return Promise.all(walkFiles(payload.tree)) }
 
-    async function walkFiles(file) {
-        if (file.children && file.children.length) {
-            await Promise.all(file.children.map(walkFiles))
-        }
+    function walkFiles(file) {
+        const promises = file.children && file.children.length ?
+            file.children.reduce((prev, curr) => prev.concat(walkFiles(curr)), []) : []
         if (options.extensions.includes(file.ext)) {
-            const body = require('fs').readFileSync(file.absolutePath, 'utf8')
-            const match = body.match(/^---(((.)|[\s\S])+?)^---/m)            
-            if (match) file.meta.frontmatter = YAML.parse(match[1])
+            promises.push(extract(file))
+        }
+        return promises;
+    }
+
+    async function extract(file) {
+        const body = await read(file.absolutePath)
+        for (let m = 0; m < matchers.length; m += 1) {
+            const match = body.match(matchers[m].pattern)
+            if (match) {
+                file.meta.frontmatter = await options.parse(match[1], matchers[m].open, matchers[m].close)
+                break;
+            }
         }
     }
 }
